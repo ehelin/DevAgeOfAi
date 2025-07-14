7817f90fd2ab4033f72eb66cb2f0209212f2422e
Eric Helin <erichelinse@gmail.com>
2025-07-14 16:25:37 -0500

    simulated habit file to jump start agent actions.

--- Shared/Constants.cs
+++ Shared/Constants.cs
@@ -5,11 +5,11 @@
         //public const string HABIT_TO_TRACK_PROMPT = "Please suggest a habit that can be tracked";
         public const string HABIT_TO_TRACK_PROMPT = "Provide a habit to track only without description.";
 
-        public const string DataFile = "data.json";
-        public const string SuggestionsFile = "suggestedHabits.json";
-        public const string HistorianStateFile = "historian.json";
-        public const string StrategistStateFile = "strategist.json";
-        public const string BuilderStateFile = "builder.json";
-        public const string AgentActivityLogFile = "agent_log.txt";
+        public const string DataFile = "Data/data.json";
+        public const string SuggestionsFile = "Data/suggestedHabits.json";
+        public const string HistorianStateFile = "Data/historian.json";
+        public const string StrategistStateFile = "Data/strategist.json";
+        public const string BuilderStateFile = "Data/builder.json";
+        public const string AgentActivityLogFile = "Data/agent_log.txt";
     }
 }
